4036d4b4148004916d4e070fe6c132267df0a260
bae <1988_b@naver.com>
2023-07-08 12:05:53 +0900

    main 페이지 반응형 완료

--- src/js/review-caroucel.js
+++ src/js/review-caroucel.js
@@ -1,16 +1,27 @@
 const reviewCaroucel = new Swiper(".review__caroucel", {
   loop: true,
-  slidesPerView: 3,
-  spaceBetween: 40,
+  slidesPerView: 1,
+  spaceBetween: 20,
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
   },
   pagination: {
     el: ".caroucel__pagination",
+    clickable: true,
   },
   navigation: {
     nextEl: ".caroucel__next",
     prevEl: ".caroucel__prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2, //브라우저가 768보다 클 때
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3, //브라우저가 1024보다 클 때
+      spaceBetween: 40,
+    },
   },
 });
